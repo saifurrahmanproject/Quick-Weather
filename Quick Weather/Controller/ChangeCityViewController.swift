d3cd6905222df1cecbe302a3bf11f07f1d482fd0
saifurrahmanproject <s.r.tonoy.p@gmail.com>
2020-11-10 15:05:10 -0500

    2nd Commit - Main Storyboard add layout and constrains and images.

---
+++ Quick Weather/Controller/ChangeCityViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ChangeCityViewController.swift
+//  Quick Weather
+//
+//  Created by Tonoy Rahman on 2020-11-09.
+//
+
+
+import UIKit
+
+class ChangeCityViewController: UIViewController {
+    
+
+    
+    @IBOutlet weak var changeCityTextField: UITextField!
+   
+    @IBAction func getWeatherPressed(_ sender: AnyObject) {
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    @IBAction func backButtonPressed(_ sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
